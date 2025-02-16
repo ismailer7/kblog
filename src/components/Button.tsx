2e90e6c7be75bab3cd3ad3cd12c5d9fc3794784b
Ismail Rzouki <ismailrzouki7@gmail.com>
2025-02-16 16:14:03 +0100

    add changes

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -28,7 +28,7 @@ export function Button({
 )) {
   className = clsx(
     className,
-    'group relative isolate flex-none rounded-md py-1.5 text-[0.8125rem]/6 font-semibold bg-sky-400 text-black',
+    'group relative isolate flex-none rounded-md py-1.5 text-[0.8125rem]/6 font-semibold bg-sky-400 text-black no-underline',
     arrow ? 'pl-2.5 pr-[calc(9/16*1rem)]' : 'px-2.5',
   )
 
